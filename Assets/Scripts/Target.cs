33772107a2be961ba3cc903c23ae2a3184479252
Bruno Henrique <brunohbm23@gmail.com>
2023-08-28 22:00:02 -0300

    Importing initial project and adding Targets Prefabs

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    float xRange = 4;
+    float ySpawnPos = -1.5f;
+    float minSpeed = 12;
+    float maxSpeed = 16;
+    float maxTorque = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        transform.position = RandomSpawnPos();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+
+}
